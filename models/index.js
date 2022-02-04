9ae8eed1d1bd4fd3e5da64dfc498475b52aa564a
Chris <cangalet@gmail.com>
2022-02-03 17:30:07 -0800

    pushing to repo

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
-// create associations
+// create associations for Posts
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
@@ -10,4 +11,33 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+// create associations for Post/Users through Votes
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'votes_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
